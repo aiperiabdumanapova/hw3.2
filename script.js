6b5cfd72553b5d52ac44a1a4bf8562d29831da08
unknown <you@example.com>
2024-04-01 21:19:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,649 @@
+const aiperi = {
+    name: 'Aiperi',
+    lastName: 'Abdumanapova',
+    age: 17,
+    isMarried: false,
+    language: ['ru', 'kg', 'en', 'cn'],
+    hobby: [
+        {
+            name: 'reading',
+            isUseful: true
+        },
+        {
+            name: 'movies',
+            isUseful: false
+        }
+    ],
+    address: {
+        street: 'Kurmanjan Datka',
+        home: 121,
+        city: 'Osh',
+        country: 'Kyrgyzstan'
+    },
+    friends: [
+        {
+            name: 'Kanykey',
+            age: 18,
+            lastName: 'Saipbaeva',
+            isMarried: false,
+            language: ['ru', 'kg', 'en', 'hu'],
+            hobby: [
+                {
+                    name: 'drawing',
+                    isUseful: true
+                },
+                {
+                    name: 'sleeping',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'Kulatov',
+                home: 5,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Aidai',
+            age: 18,
+            lastName: 'Tologonova',
+            isMarried: false,
+            language: ['ru', 'kg', 'en', 'kz'],
+            hobby: [
+                {
+                    name: 'volleyball',
+                    isUseful: true 
+                },
+                {
+                    name: 'mobile games',
+                    isUseful: false
+                }
+            ],
+            addres: {
+                street: 'Anar',
+                home: 12,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            },
+        },
+        {
+            name: 'Gulzhan',
+            age: 18,
+            lastName: 'Abdimitalipova',
+            isMarried: false,
+            language: ['ru', 'kg', 'en'],
+            hobby: [
+                {
+                    name: 'piano',
+                    isUseful: true 
+                },
+                {
+                    name: 'mobile games',
+                    isUseful: false
+                }
+            ],
+            addres: {
+                street: 'Toloykon',
+                home: 8,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            },
+        },
+        {
+            name: 'Asel',
+            age: 18,
+            lastName: 'Abdylsaky kyzy',
+            isMarried: false,
+            language: ['ru', 'kg', 'en', 'kr'],
+            hobby: [
+                {
+                    name: 'drawing',
+                    isUseful: true 
+                },
+                {
+                    name: 'anime',
+                    isUseful: false
+                }
+            ],
+            addres: {
+                street: 'South East',
+                home: 2,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            },
+        },
+        {
+            name: 'Ayana',
+            age: 19,
+            lastName: 'Amirbekova',
+            isMarried: false,
+            language: ['ru', 'kg', 'en', 'tr'],
+            hobby: [
+                {
+                    name: 'singing',
+                    isUseful: true 
+                },
+                {
+                    name: 'shopping',
+                    isUseful: false
+                }
+            ],
+            addres: {
+                street: 'Geologorodok',
+                home: 2,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            },
+        },
+        {
+            name: 'Gulumzhan',
+            age: 18,
+            lastName: 'Rakhmanberdieva',
+            isMarried: false,
+            language: ['ru', 'kg', 'en', 'cn'],
+            hobby: [
+                {
+                    name: 'dancing',
+                    isUseful: true 
+                },
+                {
+                    name: 'shopping',
+                    isUseful: false
+                }
+            ],
+            addres: {
+                street: 'Geologorodok',
+                home: 7,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            },
+        },
+        {
+            name: 'Sergay',
+            age: 19,
+            lastName: 'Kan',
+            isMarried: false,
+            language: ['ru', 'kr', 'en'],
+            hobby: [
+                {
+                    name: 'dancing',
+                    isUseful: true 
+                },
+                {
+                    name: 'drugs',
+                    isUseful: false
+                }
+            ],
+            addres: {
+                street: 'Isanova',
+                home: 7,
+                city: 'Bishkek',
+                country: 'Kyrgyzstan'
+            },
+        },
+    ],
+    acquaintances:[
+        {
+            name: 'Bekbolsun',
+            age: 25,
+            isMarried: false,
+            language: ['kg', 'cn'],
+            hobby: [
+                {
+                    name: 'photography',
+                    isUseful: true
+                },
+                {
+                    name: 'smoking',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'Lenina',
+                home: 10,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Maria',
+            age: 19,
+            isMarried: false,
+            language: ['ru', 'en'],
+            hobby: [
+                {
+                    name: 'cooking',
+                    isUseful: true
+                },
+                {
+                    name: 'drinking',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'Masalieva',
+                home: 20,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Bayel',
+            age: 18,
+            isMarried: false,
+            language: ['kg', 'ru', 'cn'],
+            hobby: [
+                {
+                    name: 'playing guitar',
+                    isUseful: true
+                },
+                {
+                    name: 'cursing',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'South East',
+                home: 15,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Elena',
+            age: 28,
+            isMarried: true,
+            language: ['ru', 'en'],
+            hobby: [
+                {
+                    name: 'yoga',
+                    isUseful: true
+                },
+                {
+                    name: 'robbing',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'Zainabedinova',
+                home: 30,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Bek',
+            age: 18,
+            isMarried: false,
+            language: ['ru', 'kg'],
+            hobby: [
+                {
+                    name: 'cooking',
+                    isUseful: true
+                },
+                {
+                    name: 'praying',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'Osh region',
+                home: 25,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Olga',
+            age: 26,
+            isMarried: false,
+            language: ['ru', 'en'],
+            hobby: [
+                {
+                    name: 'painting',
+                    isUseful: true
+                },
+                {
+                    name: 'crying',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'Kulatov',
+                home: 40,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Abdulaziz',
+            age: 18,
+            isMarried: false,
+            language: ['ru', 'kg', 'uz'],
+            hobby: [
+                {
+                    name: 'cooking',
+                    isUseful: true
+                },
+                {
+                    name: 'lying',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'Dostuk',
+                home: 25,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+ ],
+    family: [
+        {
+            name: 'Aibiyke',
+            relation: 'sister',
+            age: 12,
+            isMarried: false,
+            language: ['ru', 'kg', 'en'],
+            hobby: [
+                {
+                    name: 'painting',
+                    isUseful: true
+                },
+                {
+                    name: 'yapping',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'Kurmanjan Datka',
+                home: 121,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Zhyldyz',
+            relation: 'mother',
+            age: 39,
+            isMarried: true,
+            language: ['ru', 'kg', 'tr'],
+            hobby: [
+                {
+                    name: 'gardening',
+                    isUseful: true
+                },
+                {
+                    name: 'series',
+                    isUseful: false
+                }
+            ],
+            address: {
+                street: 'Kurmanjan Datka',
+                home: 121,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Alisher',
+            relation: 'father',
+            age: 42,
+            isMarried: true,
+            language: ['ru', 'kg'],
+            hobby: [
+                {
+                    name: 'sleeping',
+                    isUseful: false
+                },
+                {
+                    name: 'gardening',
+                    isUseful: true
+                }
+            ],
+            address: {
+                street: 'Kurmanjan Datka',
+                home: 121,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        },
+        {
+            name: 'Zhumash',
+            relation: 'grandfather',
+            age: 69,
+            isMarried: true,
+            language: ['ru', 'kg', 'gm'],
+            hobby: [
+                {
+                    name: 'billiard',
+                    isUseful: false
+                },
+                {
+                    name: 'gardening',
+                    isUseful: true
+                }
+            ],
+            address: {
+                street: 'Geolog town',
+                home: 2,
+                city: 'Osh',
+                country: 'Kyrgyzstan'
+            }
+        }
+    ],
+    movies: [
+        {
+            title: 'Inception',
+            year: 2010,
+            rating: 8.8,
+            isPopular: true,
+            isGood: false,
+            country: 'USA',
+            recommend: false
+        },
+        {
+            title: 'The Shawshank Redemption',
+            year: 1994,
+            rating: 9.3,
+            isPopular: false,
+            isGood: true,
+            country: 'France',
+            recommend: true
+        },
+        {
+            title: 'Interstellar',
+            year: 2014,
+            rating: 8.6,
+            isPopular: true,
+            isGood: false,
+            country: 'Ireland',
+            recommend: true
+        },
+        {
+            title: 'Pulp Fiction',
+            year: 1994,
+            rating: 8.9,
+            isPopular: false,
+            isGood: true,
+            country: 'USA',
+            recommend: true
+        },
+        {
+            title: 'Pearl',
+            year: 2024,
+            rating: 9,
+            isPopular: true,
+            isGood: false,
+            country: 'Uganda',
+            recommend: false
+        },
+        {
+            title: 'X Factor',
+            year: 2021,
+            rating: 8.7,
+            isPopular: true,
+            isGood: true,
+            country: 'Palestine',
+            recommend: false
+        },
+        {
+            title: 'Possesion',
+            year: 1971,
+            rating: 4,
+            isPopular: false,
+            isGood: true,
+            country: 'Italy',
+            recommend: true
+        },
+        {
+            title: 'The Glory',
+            year: 2020,
+            rating: 10,
+            isPopular: true,
+            isGood: true,
+            country: 'Republic of Korea',
+            recommend: true
+        },
+        {
+            title: 'The Red Apple',
+            year: 1970,
+            rating: 6.8,
+            isPopular: false,
+            isGood: true,
+            country: 'Kyrgyzstan',
+            recommend: true
+        }
+
+
+    ],
+    music: [
+        {
+            artist: 'Elita',
+            age: 31,
+            genre: 'Pop',
+            isPopular: false,
+            isGood: true,
+            address: {
+                street: 'High Street',
+                home: 5,
+                city: 'London',
+                country: 'UK'
+            }
+        },
+        {
+            artist: 'Beyoncé',
+            age: 40,
+            genre: 'R&B',
+            isPopular: true,
+            isGood: true,
+            address: {
+                street: 'Broadway',
+                home: 15,
+                city: 'Los Angeles',
+                country: 'USA'
+            }
+        },
+        {
+            artist: 'Kendrick Lamar',
+            age: 35,
+            genre: 'Hip-Hop',
+            isPopular: true,
+            isGood: false,
+            address: {
+                street: 'Compton Avenue',
+                home: 16,
+                city: 'Los Angeles',
+                country: 'USA'
+            }
+        },
+        {
+            artist: 'Taylor Swift',
+            age: 34,
+            genre: 'Pop',
+            isPopular: true,
+            isGood: false,
+            address: {
+                street: 'Music Row',
+                home: 10,
+                city: 'Nashville',
+                country: 'USA'
+            }
+        },
+        {
+            artist: 'Lana Del Rey',
+            age: 38,
+            genre: 'Pop',
+            isPopular: true,
+            isGood: true,
+            address: {
+                street: 'Ocean Blvd',
+                home: 10,
+                city: 'New York',
+                country: 'USA'
+            }
+        }
+    ],
+    designers: [
+        {
+          designer: 'Vivienne Westwood',
+          year: 1956,
+          style: 'punk',
+          isMarried: false,
+          isPopular: true,
+          isInfluentia: true,
+          address: {
+            street: 'Fifth Blvd',
+                home: 10,
+                city: 'London',
+                country: 'UK'
+          }
+        },
+        {
+            designer: 'Stefano Gabbana',
+            year: 1965,
+            style: 'couture',
+            isMarried: false,
+            isPopular: true,
+            isInfluentia: true,
+            address: {
+              street: 'Pesolini',
+                  home: 16,
+                  city: 'Milan',
+                  country: 'Italy'
+            }
+        },
+        {
+            designer: 'Mirror Palais',
+            year: 2000,
+            style: 'etheral',
+            isMarried: false,
+            isPopular: false,
+            isInfluentia: false,
+            address: {
+              street: 'Upper East Side',
+                  home: 17,
+                  city: 'New York',
+                  country: 'USA'
+            }
+        },
+        {
+            designer: 'Kuurchaq',
+            year: 2019,
+            style: 'kyrgyz',
+            isMarried: true,
+            isPopular: false,
+            isInfluentia: false,
+            address: {
+              street: 'Kyivskaya',
+                  home: 17,
+                  city: 'Bishkek',
+                  country: 'Kyrgyzstan'
+            }
+        },
+
+    ]
+};
+
+console.log(aiperi.friends[0].hobby[0].name);
